ab2991d60cc04be37e06011e5093d86ce2f89ec8
Mateo <mr.mateofleitas+github@gmail.com>
2017-08-01 15:29:06 -0300

    Create Ejercicio1.cpp

---
+++ Semana 01/Ejercicio1.cpp
@@ -0,0 +1,44 @@
+/* El usuario ingresa 3 valores(hora, minuto y segundo). Se debe retornar el segundo siguiente utilizando el sistema sexagesimal.
+
+#include <stdio.h>
+
+int main(){
+  int hora, minuto, segundo;
+  
+  prtinf("Ingrese hora: ");
+  scanf("%d",&hora);
+  prtinf("Ingrese minuto: ");
+  scanf("%d",&minuto);
+  prtinf("Ingrese segundo: ");
+  scanf("%d",&segundo);
+  
+  segundo++;
+  if(segundo==60){
+    segundo=0;
+    minuto++;
+  }
+  if(minuto==60){
+    minuto=0;
+    hora++;
+  }
+  if(hora==24){
+    hora=0;
+  }
+  if(hora<10){
+    printf("0%d",hora);
+  }else{
+    printf("%d",hora);
+  };
+  if(minuto<10){
+    printf("0%d",minuto);
+  }else{
+    printf("%d",minuto);
+  };
+  if(segundo<10){
+    printf("0%d",segundo);
+  }else{
+    printf("%d",segundo);
+  };
+  
+  return 0;
+}
