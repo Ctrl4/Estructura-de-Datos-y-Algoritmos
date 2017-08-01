c2d385a746a8509d96480e9091c63027f3006136
Mateo <mr.mateofleitas+github@gmail.com>
2017-08-01 15:43:06 -0300

    Create Ejercicio3.cpp

---
+++ Semana 01/Ejercicio3.cpp
@@ -0,0 +1,26 @@
+/* Se desea escribir un programa que calcule la cantidad de aprobados y reprobados de un curso. La condición para aprobar el curso es obtener al menos 60 puntos entre la suma de los dos parciales. No se conoce la cantidad exacta de alumnos, pero se sabe que posee al menos un inscripto. El programa irá leyendo los datos por teclado y finalmente mostrará por pantalla la cantidad de aprobados y reprobados.*/
+
+int main(){
+    int primerParcial, segundoParcial;
+    int notaFinal=0;
+    int contadorAprobados=0;
+    int contadorReprobados=0;
+    
+    do{
+        printf("Ingrese nota del primer parcial: ");
+        scanf("%d", &primerParcial);
+        printf("Ingrese nota del segundo parcial: ");
+        scanf("%d", &segundoParcial);
+        nota = primerParcial + segundoParcial;
+        if(nota>=60){
+            contadorAprobados++;
+        }
+        else{
+            contadorReprobados++;
+        };
+    }while(primerParcial!=-1||segundoParcial!=-1);
+    printf("Aprobados: %d",contadorAprobados);
+    printf("Reprobados: %d",contadorReprobados);
+    return 0;
+  
+}
